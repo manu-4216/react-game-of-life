b85e62971698b2a6e614eda07ff3384423e8d6e4
Manuel Micu <manu4216@gmail.com>
2016-10-04 21:07:22 +0200

    refactored into stateless func components and containers

--- app/components/Content.js
+++ app/components/Content.js
@@ -5,19 +5,21 @@ require('../main.scss');
 
 var Content = function(props) {
   var genNr = 0;
+
   return (
     <div className='content'>
       <div className='game-top'>
         <Button>Run</Button>
         <Button>Pause</Button>
         <Button>Clear</Button>
+        <Button>NEXT</Button>
         <span>Generation: {genNr}</span>
       </div>
 
       <GameBoard />
 
       <div className='game-bottom'>
-        <div>Board Size:<Button>50x30</Button><Button>70x50</Button><Button>100x80</Button></div>
+        <div>Cell size:<Button>Small</Button><Button>Medium</Button><Button>Big</Button></div>
         <div>Sim Speed:<Button>Slow</Button><Button>Medium</Button><Button>Fast</Button></div>
       </div>
 
--- app/components/GameBoard.js
+++ app/components/GameBoard.js
@@ -1,85 +1,102 @@
 var React = require('react');
+var CellsGrid = require('../components/CellsGrid');
 require('../main.scss');
 
-var GameBoard = function(props) {
+var GameBoard = React.createClass({
 
-  var size = {
-    width: '40rem',
-    height: '24rem'
-  };
+  getInitialState: function () {
+    return {
+      aliveCells: [1, 3, 78]
+    }
+  },
 
-  function getItems() {
-    let cells = [];
-    for (let i = 1; i < 50*30 + 1; i++) {
-        cells.push(<span className='cell'
-                          key={i}
-                          onClick={() => {
-                            console.log('pressed: ' + i);
-                            getNeighbors(i, 50, 30);
-                          }}>
-                          {i}
-                  </span>);
-      }
-    return cells;
+  render: function () {
+    return (
+      <CellsGrid
+        aliveCells={this.state.aliveCells}
+        onClick={this.onClick}
+      />
+    );
+  },
+
+
+  onClick: (index) => {
+       console.log('pressed: ' + index);
+       const getNeighbors = (index, gameWidth, gameHeight) => {
+           let neighbors = [];
+           let checkedPosition;
+           // Find the neighbor on top-left:
+           checkedPosition = (index - gameWidth - 1);
+           if (checkedPosition > 0 && (checkedPosition%gameWidth !== 0)) {
+             neighbors.push(checkedPosition);
+           }
+           // Find the neighbor right on top:
+           checkedPosition = (index - gameWidth);
+           if (checkedPosition > 0) {
+             neighbors.push(checkedPosition);
+           }
+           // Find the neighbor on top-right:
+           checkedPosition = (index - gameWidth + 1);
+           if ((checkedPosition > 0) && (index%gameWidth !== 0)) {
+             neighbors.push(checkedPosition);
+           }
+
+           // Find the neighbor on the left: --------------------------------------
+           checkedPosition = (index - 1);
+           if (checkedPosition > 0 && (checkedPosition%gameWidth !== 0)) {
+             neighbors.push(checkedPosition);
+           }
+           // Find the neighbor on the right:
+           checkedPosition = (index + 1);
+           if (index%gameWidth !== 0) {
+             neighbors.push(checkedPosition);
+           }
+
+           // Find the neighbor on bottom-left: -----------------------------------
+           checkedPosition = (index + gameWidth - 1);
+           if ((checkedPosition < gameWidth * gameHeight) && (checkedPosition%gameWidth !== 0)) {
+             neighbors.push(checkedPosition);
+           }
+           // Find the neighbor right on bottom:
+           checkedPosition = (index + gameWidth);
+           if (checkedPosition <= gameWidth * gameHeight) {
+             neighbors.push(checkedPosition);
+           }
+           // Find the neighbor on bottom-right:
+           checkedPosition = (index + gameWidth + 1);
+           if ((checkedPosition <= gameWidth * gameHeight) && (index%gameWidth !== 0)) {
+             neighbors.push(checkedPosition);
+           }
+
+           console.log("n="+neighbors);
+           return neighbors;
+       };
+
+       const reversedCellState = (index, aliveCells) => {
+         let updatedAliveCells = [];
+         let positionOfIndexedCell = aliveCells.indexOf(index);
+         // if alive, kill, by returning only the items that are different:
+         if (positionOfIndexedCell >= 0) {
+           updatedAliveCells = aliveCells.filter(item => (item !== index));
+         } else {
+           // If non existent, create cell:
+           updatedAliveCells = aliveCells.concat(index);
+           console.log("Added " + index);
+         }
+
+         // Return the new state:
+         return updatedAliveCells;
+       };
+
+       getNeighbors(index, 50, 30);
+       //reverseCellState.bind(null, index, aliveCells)();
+
+       /*this.setState({
+         aliveCells: reverseCellState(index, this.state.aliveCells)
+       })*/
+       //this.forceUpdate();
   }
-
-  function getNeighbors(index, gameWidth, gameHeight) {
-    let neighbors = [];
-    let checkedPosition;
-    // Find the neighbor on top-left:
-    checkedPosition = (index - gameWidth - 1);
-    if (checkedPosition > 0 && (checkedPosition%gameWidth !== 0)) {
-      neighbors.push(checkedPosition);
-    }
-    // Find the neighbor right on top:
-    checkedPosition = (index - gameWidth);
-    if (checkedPosition > 0) {
-      neighbors.push(checkedPosition);
-    }
-    // Find the neighbor on top-right:
-    checkedPosition = (index - gameWidth + 1);
-    if ((checkedPosition > 0) && (index%gameWidth !== 0)) {
-      neighbors.push(checkedPosition);
-    }
-
-    // Find the neighbor on the left: --------------------------------------
-    checkedPosition = (index - 1);
-    if (checkedPosition > 0 && (checkedPosition%gameWidth !== 0)) {
-      neighbors.push(checkedPosition);
-    }
-    // Find the neighbor on the right:
-    checkedPosition = (index + 1);
-    if (index%gameWidth !== 0) {
-      neighbors.push(checkedPosition);
-    }
-
-    // Find the neighbor on bottom-left: -----------------------------------
-    checkedPosition = (index + gameWidth - 1);
-    if ((checkedPosition < gameWidth * gameHeight) && (checkedPosition%gameWidth !== 0)) {
-      neighbors.push(checkedPosition);
-    }
-    // Find the neighbor right on bottom:
-    checkedPosition = (index + gameWidth);
-    if (checkedPosition <= gameWidth * gameHeight) {
-      neighbors.push(checkedPosition);
-    }
-    // Find the neighbor on bottom-right:
-    checkedPosition = (index + gameWidth + 1);
-    if ((checkedPosition <= gameWidth * gameHeight) && (index%gameWidth !== 0)) {
-      neighbors.push(checkedPosition);
-    }
-
-    console.log("n="+neighbors);
-    return neighbors;
-  }
-
-  return (
-    <div className='game-board' style={size}>
-      <div className='div'>
-        { getItems() }
-      </div>
-    </div>
-  )
-};
+  
+});
 
 module.exports = GameBoard;
